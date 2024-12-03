392bef56458b37f7b074514f6684c59364a0e200
Md. Tipu <tipu.diu.cse@gmail.com>
2024-12-02 21:17:15 -0800

    app initials
    app initials

---
+++ Phone Guardian/Phone Guardian/Phone_GuardianApp.swift
@@ -0,0 +1,17 @@
+//
+//  Phone_GuardianApp.swift
+//  Phone Guardian
+//
+//  Created by MacBook Pro M1 Pro on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Phone_GuardianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
